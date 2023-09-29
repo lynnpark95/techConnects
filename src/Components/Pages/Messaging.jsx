dafb1c448094d9bda8f49c7905b799ef2aa1bec0
MyYodaSoda <alexander.connolly@edu.sait.ca>
2023-09-29 12:13:17 -0600

    tried to add some stuff to the mnessaging page

--- src/Components/Pages/Messaging.jsx
+++ src/Components/Pages/Messaging.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from '../Navbar Items/Navbar';
 import { Typography } from '@mui/material';
 import Box from "@mui/material/Box";
+import MessagingPage from './ChatComponents/MessagingPage';
 
 const Messaging = () => {
+  const marginValue ='500px'
     return (
-        <div style={{maginLeft: '500px'}}>
+        <div style={{maginLeft: marginValue}}>
             <Navbar />
             <Box
             sx={{
@@ -13,11 +15,13 @@ const Messaging = () => {
               alignItems: "center",
             }}
           >
-            <Typography variant="body2" component="p" marginLeft={45}>
-            Messaging Test
+            <Typography variant="h3" component="h1" marginLeft={45} marginTop={5}>
+            Messaging
             </Typography>
           </Box>
+          <MessagingPage />
         </div>
+
     )
 }
 export default 
