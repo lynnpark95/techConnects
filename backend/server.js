139068e0176ee45d20d1fbf973b2a6c4ee9d7818
Ray Luo <122394390+SC4RL5@users.noreply.github.com>
2023-10-11 10:29:04 -0600

    Added Pusher functionality to the backend.

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
+const Pusher = require('pusher');
 const userRoutes = require('./routes/user')
 const messageRoutes = require('./routes/message')
+const { setupPusher } = require('./services/pusherService')
 
 // express app
 const app = express()
@@ -27,6 +29,7 @@ app.use('/api/message', messageRoutes)
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
+    setupPusher(mongoose.connection);
     // listen for requests
     app.listen(process.env.PORT, () => {
       console.log('connected to db & listening on port', process.env.PORT)
@@ -34,4 +37,33 @@ mongoose.connect(process.env.MONGO_URI)
   })
   .catch((error) => {
     console.log(error)
+  })
+
+// Pusher
+const pusher = new Pusher({
+  appId: "1685414",
+  key: "cb78a53cfed82eb19581",
+  secret: "c52e331e45f7964616d4",
+  cluster: "us3",
+  useTLS: true
+});
+
+const db = mongoose.connection
+  db.once('open', () => {
+    console.log('DB Connected to Pusher')
+    const msgCollection = db.collection('messages')
+    const changeStream = msgCollection.watch()
+    changeStream.on('change', (change) => {
+      if (change.operationType === 'insert') {
+        const messageDetails = change.fullDocument;
+        pusher.trigger('messages', 'inserted', {
+          name: messageDetails.name,
+          message: messageDetails.message,
+          timestamp: messageDetails.timestamp,
+          received: messageDetails.received,
+        });
+      } else {
+        console.log('Error triggering Pusher')
+      }
+    })
   })
