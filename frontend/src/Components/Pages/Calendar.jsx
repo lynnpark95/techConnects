733d8ad28a40f98729211bc23bc219bc3002a890
John Enoch S. Crudo <johncrudo24@gmail.com>
2023-10-30 12:04:49 -0600

    John

--- frontend/src/Components/Pages/Calendar.jsx
+++ frontend/src/Components/Pages/Calendar.jsx
@@ -1,23 +1,72 @@
 import React from 'react';
+import Header from "./../Header";
 import Navbar from '../Navbar Items/Navbar';
 import { Typography } from '@mui/material';
 import Box from "@mui/material/Box";
+import './Calendar.css';
 
 const Calendar = () => {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+
+    const generateCalendarDays = () => {
+        const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+        const firstDay = new Date(currentYear, currentMonth, 1).getDay();
+        
+       
+        const startingDay = firstDay === 0 ? 6 : firstDay - 1;
+
+        const calendarDays = [];
+
+        for (let i = 0; i < startingDay; i++) {
+            calendarDays.push(null);
+        }
+
+        for (let day = 1; day <= daysInMonth; day++) {
+            calendarDays.push(day);
+        }
+
+        return calendarDays;
+    }
+
     return (
-        <div style={{maginLeft: '500px'}}>
+        <div className="calendar-container">
+            <Header />
             <Navbar />
             <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="body2" component="p" marginLeft={45}>
-            Calendar Test
-            </Typography>
-          </Box>
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography variant="body2" component="p" className="calendar-title">
+                    {months[currentMonth]} {currentYear} Calendar
+                </Typography>
+
+                <div className="calendar">
+                    <div className="weekdays">
+                        {daysOfWeek.map((day, index) => (
+                            <div key={index} className="weekday">
+                                {day}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="days">
+                        {generateCalendarDays().map((day, index) => (
+                            <div key={index} className="day">
+                                {day !== null ? day : ''}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </Box>
         </div>
     )
 }
+
 export default Calendar;
