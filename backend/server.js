a1783a82fe1821c0c970255d5789ddef93de8671
Ray Luo <122394390+SC4RL5@users.noreply.github.com>
2023-11-10 18:36:51 -0700

    Removed previous User authentication/authorization

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,18 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
 
 // middleware
 app.use(express.json())
-app.use(cookieParser())
 app.use(cors())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
-
-// routes
-app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
