f88752a4aead487bbcb80fdcf61184f78d459b79
John Enoch S. Crudo <johncrudo24@gmail.com>
2023-11-20 12:42:09 -0700

    Update

--- frontend/src/Components/Pages/CalendarComponents/Calendar.jsx
+++ frontend/src/Components/Pages/CalendarComponents/Calendar.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import Header from "../../Header";
-import Navbar from '../../Navbar Items/Navbar';
-import { Typography } from '@mui/material';
-import Box from "@mui/material/Box";
+import React from "react";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 
 const Calendar = () => {
-  const [value, setValue] = React.useState(dayjs('2023-11-10'));
+  const [value, setValue] = React.useState(dayjs("2022-04-07"));
 
-  <LocalizationProvider dateAdapter={AdapterDayjs}>
-  <StaticDatePicker
-    orientation="landscape"
-    openTo="day"
-    value={value}
-    onChange={(newValue) => {
-      setValue(newValue);
-    }}
-    renderInput={(params) => <TextField {...params} />}
-  />
-</LocalizationProvider>
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDatePicker
+        orientation="landscape"
+        openTo="day"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
 };
-
-
 
 export default Calendar;
