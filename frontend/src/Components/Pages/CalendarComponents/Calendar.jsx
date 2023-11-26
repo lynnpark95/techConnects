d753844fca2721caa4393ac7f8396dbb7cf9e6bb
John Enoch S. Crudo <johncrudo24@gmail.com>
2023-11-25 23:12:44 -0700

    merging

--- frontend/src/Components/Pages/CalendarComponents/Calendar.jsx
+++ frontend/src/Components/Pages/CalendarComponents/Calendar.jsx
@@ -1,30 +1,4 @@
 import React from "react";
-import dayjs from "dayjs";
-import TextField from "@mui/material/TextField";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
-
-const Calendar = () => {
-  const [value, setValue] = React.useState(dayjs("2022-04-07"));
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker
-        orientation="landscape"
-        openTo="day"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-      />
-    </LocalizationProvider>
-  );
-};
-
-export default Calendar;
-import React from "react";
 import Navbar from "../../Navbar Items/Navbar";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
