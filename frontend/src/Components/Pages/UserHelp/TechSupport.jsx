5b831175e2fe94c3b28d5e586c51aab8bf9c8f4c
MyYodaSoda <alexander.connolly@edu.sait.ca>
2023-11-29 15:18:40 -0700

    With calendar

--- frontend/src/Components/Pages/UserHelp/TechSupport.jsx
+++ frontend/src/Components/Pages/UserHelp/TechSupport.jsx
@@ -61,7 +61,7 @@ const TechSupport = () => {
       <Typography
         variant="h7"
         component="p"
-        marginTop={10}
+        marginTop={2}
         marginBottom={5}
         fontSize={"45px"}
         style={{
