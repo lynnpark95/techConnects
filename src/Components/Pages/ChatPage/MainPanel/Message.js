fceeaf350335152cc8463f48a9701aa46d621c85
John Enoch S. Crudo <johncrudo24@gmail.com>
2023-11-29 14:46:08 -0700

    Working on the messaging bubble

--- src/Components/Pages/ChatPage/MainPanel/Message.js
+++ src/Components/Pages/ChatPage/MainPanel/Message.js
@@ -1,32 +1,49 @@
-import React from 'react'
+import React from 'react';
 import moment from 'moment';
 
 function Message({ message, user }) {
-
     const timeFromNow = timestamp => moment(timestamp).fromNow();
 
     const isImage = message => {
         return message.hasOwnProperty("image") && !message.hasOwnProperty("content");
-    }
+    };
+
     const isMessageMine = (message, user) => {
-       if(user) {
-           return message.user.id === user.uid
-       }
-    }
+        if (user) {
+            return message.user.id === user.uid;
+        }
+    };
+
+    const messageContainerStyle = {
+        marginBottom: '3px',
+        display: 'flex',
+        flexDirection: isMessageMine(message, user) ? 'row-reverse' : 'row',
+    };
+
+    const userImageStyle = {
+        borderRadius: '10px',
+        marginLeft: isMessageMine(message, user) ? '0' : '10px',
+        marginRight: isMessageMine(message, user) ? '10px' : '0',
+    };
+
+    const messageContentStyle = {
+        backgroundColor: isMessageMine(message, user) ? "#ADD8E6" : "#ECECEC",
+        textAlign: isMessageMine(message, user) ? 'right' : 'left',
+        padding: '10px',
+        borderRadius: '10px',
+    };
 
     return (
-        <div style={{ marginBottom: '3px', display:'flex' }}>
+        <div style={messageContainerStyle}>
             <img
-                style={{ borderRadius: '10px' }}
+                style={userImageStyle}
                 width={48}
                 height={48}
                 className="mr-3"
                 src={message.user.image}
                 alt={message.user.name}
             />
-            <div style={{
-                backgroundColor: isMessageMine(message, user) && "#ECECEC"
-            }}>
+            <div style={messageContentStyle}>
                 <h6>{message.user.name}{" "}
                     <span style={{ fontSize: '10px', color: 'gray' }}>
                         {timeFromNow(message.timestamp)}
@@ -41,7 +58,7 @@ function Message({ message, user }) {
                 }
             </div>
         </div>
-    )
+    );
 }
 
-export default Message
+export default Message;
